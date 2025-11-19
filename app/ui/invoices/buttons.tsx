024a79e8938e83f8cd539d68de48daf937a52986
aurora8934 <70012098+aurora8934@users.noreply.github.com>
2025-11-19 23:07:55 +0100

    Fix
    Form bug

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -26,14 +26,16 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId=deleteInvoice.bind(null, id);
+  const deleteInvoiceWithId = async (formData: FormData) => {
+    await deleteInvoice(id);
+  };
   return (
     <>
       <form action={deleteInvoiceWithId}>
         <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
-      </button>
+          <span className="sr-only">Delete</span>
+          <TrashIcon className="w-5" />
+        </button>
       </form>
     </>
   );
